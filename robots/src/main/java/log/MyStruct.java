85b2a2333397d69863264cb399bfd972e02ce1ba
Khlff <n.hlopunov@ya.ru>
2023-05-05 17:02:39 +0500

    Iterator for Log struct created

--- robots/src/main/java/log/MyStruct.java
+++ robots/src/main/java/log/MyStruct.java
@@ -1,10 +1,12 @@
 package log;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-
-public class MyStruct {
+public class MyStruct implements Iterable<LogEntry> {
     private int structureSize;
     private Queue<LogEntry> queue;
 
@@ -19,7 +21,6 @@ public class MyStruct {
         }
         queue.add(element);
     }
-
 
     public List<LogEntry> getSlice(int startIndex, int endIndex) {
         List<LogEntry> list = new ArrayList<>(queue);
@@ -38,8 +39,30 @@ public class MyStruct {
         return this.queue.size();
     }
 
-    public Iterable<LogEntry> getIter() {
-        Iterable<LogEntry> iterable = queue;
-        return iterable;
+    @Override
+    public Iterator<LogEntry> iterator() {
+        return new MyStructIterator(queue.iterator());
+    }
+
+    private class MyStructIterator implements Iterator<LogEntry> {
+        private Iterator<LogEntry> queueIterator;
+
+        public MyStructIterator(Iterator<LogEntry> queueIterator) {
+            this.queueIterator = queueIterator;
+        }
+
+        @Override
+        public boolean hasNext() {
+            synchronized (queue) {
+                return queueIterator.hasNext();
+            }
+        }
+
+        @Override
+        public LogEntry next() {
+            synchronized (queue) {
+                return queueIterator.next();
+            }
+        }
     }
 }
